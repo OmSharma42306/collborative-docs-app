5098834da2d3188a8c4123e43086bdfbb6953e4e
Om Sharma <omsharma.83173@gmail.com>
2025-03-07 23:40:44 +0530

    added-websocket-layer-with-initial-setup

---
+++ backend/src/index.ts
@@ -0,0 +1,65 @@
+import express from "express";
+import {WebSocketServer,WebSocket} from "ws";
+
+
+const wss = new WebSocketServer({port:8080});
+
+let senderWs:null | WebSocket = null;
+let receiverWs:null | WebSocket = null;
+
+wss.on('connection',function connection(ws){
+    const roomId = "123";
+    if (!ws){
+        console.error("Websocket connection Error");
+    }
+
+    ws.on('message',function message(data:any,isBinary){
+        
+        const msg = JSON.parse(data);
+        console.log(msg.roomIdofclient)
+        console.log(msg.clientType)
+        console.log(msg.roomIdofclient === roomId)
+        if(msg.roomIdofclient === roomId && msg.clientType === "sender"){
+            console.log("i am in sender!")
+            senderWs = ws;
+            console.log("i am in sender!")
+            senderWs.send("I am from sender");    
+        } 
+
+        if(msg.roomIdofclient === roomId && msg.clientType === "receiver"){
+            console.log("i am in receiver!")
+            receiverWs = ws;
+            console.log("i am in receiver!")
+            receiverWs.send("i am receiver socket.")
+
+        }        
+        
+        if(senderWs){
+            senderWs.send(JSON.stringify({msg:"YE KYA BOLTI TU < "}))
+        }
+
+        ws.send("connected!");    
+    })
+
+    console.log(senderWs)
+    console.log(receiverWs)
+        
+    if(senderWs){
+        senderWs.on('message',function message(data:any,isBinary){
+            const msg = JSON.parse(data);
+            receiverWs?.send(JSON.stringify({msg:"changeText",data}));
+        })}
+    
+    if(receiverWs){
+        receiverWs.on('message',function message(data:any,isBinary){
+            const msg = JSON.parse(data);
+            senderWs?.send(JSON.stringify({msg:"changeText",data}));
+        })
+    }
+    
+
+
+})
+
+
+
