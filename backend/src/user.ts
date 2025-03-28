f6c8fd3eeb9f7e49a850f59ee9a46f50825112e2
Om Sharma <omsharma.83173@gmail.com>
2025-03-28 23:54:34 +0530

    added-user-routes

---
+++ backend/src/user.ts
@@ -0,0 +1,13 @@
+import express from "express";
+
+
+const app = express();
+const PORT = 4000;
+
+
+// User Related Stuff..
+
+
+app.listen(4000,()=>{
+    console.log(`Server Started! at PORT : ${PORT}`)
+})
