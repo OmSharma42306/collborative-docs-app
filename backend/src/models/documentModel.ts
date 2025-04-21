5049585081891ef3184e73c95e321db378ba120a
Om Sharma <omsharma.83173@gmail.com>
2025-04-21 23:29:01 +0530

    referenced-the-document-model-to-users-model-and-addded-time-stamp

--- backend/src/models/documentModel.ts
+++ backend/src/models/documentModel.ts
@@ -1,11 +1,20 @@
 import mongoose, { Mongoose } from "mongoose";
 
 
-
 const documentSchema = new mongoose.Schema({
+    userId :{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'Users',
+        required:false
+    },
+
     documentName:{
         type:String,
         required:true
+    },
+    createdAt:{
+        type:Date,
+        default:Date.now()
     }
 });
 
