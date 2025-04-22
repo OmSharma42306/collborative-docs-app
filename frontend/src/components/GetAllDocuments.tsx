22486e0d77f0477b7c382f0edc4a4789df4a375f
Om Sharma <omsharma.83173@gmail.com>
2025-04-23 00:44:58 +0530

    added-frontend-logic-and-be-logic-to-get-all-existing-files.

--- frontend/src/components/GetAllDocuments.tsx
+++ frontend/src/components/GetAllDocuments.tsx
@@ -1,17 +1,41 @@
 import axios from "axios"
+import { useEffect, useState } from "react";
 const token = localStorage.getItem("token")
 export default function GetAllDocuments(){
+    const [allDocuments,setAllDocuments] = useState<[]>([]);
+    useEffect(()=>{
     async function fetchAllDocuments(){
         const response = await axios.get("http://localhost:3000/api/v1/docs/get-all-docs",{
             headers:{
                 Authorization:`Bearer ${token}`
             }
         });
-        console.log("Response",response)
+            console.log("Response",response)
+            const data = response.data.allDocs;
+            console.log(data)
+            setAllDocuments(data);
+        }
+        fetchAllDocuments();
+    },[])
+    
+
+    async function handleOpenDocument(){
 
     }
-    return <div>
-gggg
-<button onClick={fetchAllDocuments}>click</button>
+
+return <div>
+<h1>All Documents are Listed Below</h1>
+    {
+        allDocuments && allDocuments.length>0 ? allDocuments.map((value:any)=>{
+            return <div>
+                {value.documentName}
+                <br />
+                
+                <button onClick={handleOpenDocument}>Open the Document</button>
+                
+            </div>
+        })  :"Loading...."
+    }
+
     </div>
 }
