0c8ec6bc56e54e4737095d825b3abd82ee994db7
Om Sharma <omsharma.83173@gmail.com>
2025-04-23 01:55:39 +0530

    removed-unnecessary-recoil-root-from-main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,12 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import {BrowserRouter} from "react-router-dom"
-import { RecoilRoot } from 'recoil'
+
 createRoot(document.getElementById('root')!).render(
-    <RecoilRoot>
     <BrowserRouter>
     <App />
     </BrowserRouter>
-    </RecoilRoot>
-  
 )
